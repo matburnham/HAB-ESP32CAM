304349a388d66398bd20c9d49d5bbd11c7b67dbd
Mat Burnham <matburnham@gmail.com>
2022-09-28 22:39:28 +0100

    Initial boilerplate for ESP32 compile/test based around Blink

---
+++ Tracker.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Arduino.h>
+
+#define LED_BUILTIN 13
+
+class Tracker {
+    public:
+        void setup();
+        void loop();
+};
---
+++ examples/Tracker/Tracker.ino
@@ -0,0 +1,18 @@
+/*
+ * Tracker stub
+ * 
+*/
+
+#include "Tracker.h"
+
+Tracker tracker;
+
+void setup()
+{
+	tracker.setup();
+}
+
+void loop()
+{
+	tracker.loop();
+}
